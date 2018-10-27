a4b7144734a0ad7366ea0f002929be6c95ccbbab
Alex Malitsky <a.malitsky@gmail.com>
2018-10-27 15:40:28 +0300

    feat(insertion): first version

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+'use strict';
+
 const test = [3, 45, 6, 7, 0, 1, 21, 11, 99, 6, 7, 3];
 const test2 = [0, 1, 3, 3, 6, 6, 7, 7, 11, 21, 45, 99].reverse();
+const test3 = [0, 1, 3, 3, 6, 6, 7, 7, 11, 21, 45, 99];
 
 function swapArrElements(arr, i, j) {
   const temp = arr[i];
@@ -38,12 +41,29 @@ function selectionSort(arr) {
     }
 
     if (maxIndex !== j - 1) {
-      swapArrElements(arr, maxIndex, j - 1);
+      swapArrElements(arr, j - 1, maxIndex);
     }
   }
 
   return arr;
 }
 
-console.log(selectionSort(test2));
-console.log(selectionSort(test));
+function insertionSort(arr) {
+  const {length} = arr;
+
+  for (let j = length - 1; j >= 0; j--) {
+    if (arr[j - 1] > arr[j]) {
+      for (let i = j; i < length; i++) {
+        if (arr[i - 1] > arr[i]) {
+          swapArrElements(arr, i - 1, i);
+        }
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(insertionSort(test));
+console.log(insertionSort(test2));
+console.log(insertionSort(test3));
